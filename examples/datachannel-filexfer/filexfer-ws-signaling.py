ae0a1b99d0b303202089b2dadf039937791e7abf
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-04-29 06:31:05 +0900

    Update filexfer-ws-signaling.py

--- examples/datachannel-filexfer/filexfer-ws-signaling.py
+++ examples/datachannel-filexfer/filexfer-ws-signaling.py
@@ -5,8 +5,8 @@ import time
 import sys
 import os
 
-from os import path
-sys.path.append(path.dirname(path.abspath(__file__)) + "/../../")
+#from os import path
+#sys.path.append(path.dirname(path.abspath(__file__)) + "/../../")
 
 os.environ['AIORTC_SPECIAL_MODE'] = 'DC_ONLY'
 
